900c6e90db08b9446cfae3941893e6acd617dbcf
Michael D. Hoyle <mhoyle@freshbooks.com>
2015-09-03 15:48:04 -0400

    add fixtures and values parsing

--- stemp.py
+++ stemp.py
@@ -1,6 +1,5 @@
 import argparse
 
-
 parser = argparse.ArgumentParser(description='Render a Stemp template.')
 
 help_string = 'Path to the values file. Should be in json dictionary format'
@@ -20,7 +19,21 @@ parser.add_argument('-s', '--specification', dest='print_specification',
 
 args = parser.parse_args()
 
+import json
+from pprint import pprint
+
 if __name__ == '__main__':
     print 'values at: %s' % args.values_path
     print 'input at: %s' % (args.template_path or 'stdin')
-    print 'values at: %s' % (args.output_path or 'stdout')
+    print 'output to: %s' % (args.output_path or 'stdout')
+
+    # load values
+    with open(args.values_path) as values_file:
+        values = json.load(values_file)
+
+    pprint(values)
+    # initialize templator
+
+    # invoke templator
+
+    # output file
